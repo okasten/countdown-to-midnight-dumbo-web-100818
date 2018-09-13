274dbb59cb268c95ea654c95d44f8ecfa6791764
Olivia Kasten <odkasten@gmail.com>
2018-09-13 23:47:46 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -12,5 +12,12 @@ def countdown(num)
 end
 
 def countdown_with_sleep(num)
-  while 
+  while num >= 0 
+    if num == 0 
+      return "HAPPY NEW YEAR!"
+    else 
+      puts "#{num} SECOND(S)!"
+      num -= 1 
+    end
+  end
 end 
